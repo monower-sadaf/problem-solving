a4e694e0fd7a0d53c00528103eb981465e46bdde
monower <monower.mysoftheaven@gmail.com>
2023-10-30 12:54:26 +0600

    solved palindrome problem

--- leet-code/index.js
+++ leet-code/index.js
@@ -8,7 +8,7 @@ You can return the answer in any order.
 */
 
 
-const twoSum = (nums,target) => {
+/* const twoSum = (nums,target) => {
     let result = [];
 
     for(i=0; i <nums.length; i++){
@@ -23,4 +23,33 @@ const twoSum = (nums,target) => {
 };
 
 
-console.log(twoSum([2,7,11,15],9));
+console.log(twoSum([2,7,11,15],9)); */
+
+
+/* 
+9. Palindrome Number
+
+Given an integer x, return true if x is a
+palindrome
+, and false otherwise.
+
+*/
+
+
+const isPalindrome = (num) => {
+    let temp = 0;
+    let target = num;
+    
+    if(num<0){
+        return false;
+    }
+    while (num>0){
+        let rem = num%10;
+        temp = temp * 10 + rem;
+        num = Math.floor(num/10);
+    }
+
+    return temp === target;
+};
+
+console.log(isPalindrome(121));
